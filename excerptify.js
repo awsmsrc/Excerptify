9da9f208d2e54652dd6f4a8cfc5814f6d84b4fd0
Luke Roberts <email@luke-roberts.info>
2011-10-05 01:12:38 +0100

    demo page basic

--- excerptify.js
+++ excerptify.js
@@ -1,34 +1,39 @@
 $(function(){
-  var limit = 100;
-  var count = 0;
-  
+  $.fn.excerptify = function() {	
+    var limit = 100;
+    var count = 0;
+    
 	$('.pagepart').each(function(i, e){
-	  $excerpt = $(e).find(">:first-child")
-	  count += $excerpt.text().length
-	  while(count <= limit){
-	    if (!$excerpt.length){
-	      break
-	    }
-	    $excerpt = $excerpt.next()
-	    count += $excerpt.text().length
-	  }
-	  $excerpt.nextAll().addClass('hide_me').hide()	  
-	  
-	  if($(e).find('.hide_me').length){
-	    $(e).append('<a href="#" class="expand">read more...</a>')  
-	  }
-	  count = 0;
-	})
-	
-	
-	$('.pagepart .expand').click(function(ev){
-    ev.preventDefault()
-    if($(this).parent().find('.hide_me').is(':visible')){
-  	  $(this).parent().find('.hide_me').hide()
-      $(this).html('read more...')    	  
-    }else{
-      $(this).parent().find('.hide_me').show()
-      $(this).html('read less...')
-    }	
-  })  
-})
+      $excerpt = $(e).find(">:first-child")
+      count += $excerpt.text().length
+      while(count <= limit){
+        if (!$excerpt.length){
+          break
+        }
+        $excerpt = $excerpt.next()
+        count += $excerpt.text().length
+      }
+      $excerpt.nextAll().addClass('hide_me').hide()	  
+  
+      if($(e).find('.hide_me').length){
+        $(e).append('<a href="#" class="expand">read more...</a>')  
+      }
+      count = 0;
+    })
+
+    
+
+    //expander link click functionality and toggling
+    $('.pagepart .expand').click(function(ev){
+      ev.preventDefault()
+      if($(this).parent().find('.hide_me').is(':visible')){
+ 	    $(this).parent().find('.hide_me').hide()
+        $(this).html('read more...')    	  
+      }else{
+        $(this).parent().find('.hide_me').show()
+        $(this).html('read less...')
+      }	
+    })  
+  
+  }//end plugin function
+})//end doc ready function
